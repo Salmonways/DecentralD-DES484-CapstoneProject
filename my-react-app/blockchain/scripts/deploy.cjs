1d25c698770b52ad90b40168572e8f18b431f1ca
Nutthawee Charoenngampis <nutthawee.cha@gmail.com>
2025-05-29 15:55:27 +0700

    Update deploy script

--- my-react-app/blockchain/scripts/deploy.cjs
+++ my-react-app/blockchain/scripts/deploy.cjs
@@ -1,21 +1,3 @@
-// const hre = require("hardhat");
-
-// async function main() {
-//   const CredentialContract = await hre.ethers.getContractFactory("CredentialRegistry"); 
-//   const contract = await CredentialContract.deploy();
-
-//   await contract.waitForDeployment(); 
-
-//   const address = await contract.getAddress(); 
-
-//   console.log("✅ Contract deployed to:", address);
-// }
-
-// main().catch((error) => {
-//   console.error("❌ Deployment failed:", error);
-//   process.exit(1);
-// });
-
 const hre = require("hardhat");
 const fs = require("fs");
 const path = require("path");
