dfd568321ae61413e92696cfd74da9effc6a6062
Nutthawee Charoenngampis <nutthawee.cha@gmail.com>
2025-05-29 15:08:46 +0700

    update hardhat config

--- my-react-app/blockchain/hardhat.config.cjs
+++ my-react-app/blockchain/hardhat.config.cjs
@@ -1,13 +1,18 @@
-
 require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
+const { PRIVATE_KEY } = process.env;
+
+if (!PRIVATE_KEY) {
+  console.warn("⚠️ Missing PRIVATE_KEY in .env file");
+}
+
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
   networks: {
     sepolia: {
-      url: `https://mainnet.infura.io/v3/0ff77e19cff941d49e269e238907dd71`,
-      accounts: [process.env.PRIVATE_KEY],
+      url: "https://sepolia.infura.io/v3/0ff77e19cff941d49e269e238907dd71",
+      accounts: PRIVATE_KEY ? [`0x${PRIVATE_KEY}`] : [],
     },
   },
 };
