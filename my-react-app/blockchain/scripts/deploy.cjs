31f211c9ccca0161000b3fc561dddbb46aec8b63
Nutthawee Charoenngampis <nutthawee.cha@gmail.com>
2025-05-29 15:54:17 +0700

    Update deploy script

--- my-react-app/blockchain/scripts/deploy.cjs
+++ my-react-app/blockchain/scripts/deploy.cjs
@@ -26,35 +26,31 @@ async function main() {
   const issuerRegistry = await IssuerRegistry.deploy();
   await issuerRegistry.waitForDeployment();
   const issuerRegistryAddress = await issuerRegistry.getAddress();
-  console.log("IssuerRegistry deployed to:", issuerRegistryAddress);
 
   // Deploy UserRegistry
   const UserRegistry = await hre.ethers.getContractFactory("UserRegistry");
   const userRegistry = await UserRegistry.deploy();
   await userRegistry.waitForDeployment();
   const userRegistryAddress = await userRegistry.getAddress();
-  console.log("UserRegistry deployed to:", userRegistryAddress);
 
   // Deploy RevocationRegistry
   const RevocationRegistry = await hre.ethers.getContractFactory("RevocationRegistry");
   const revocationRegistry = await RevocationRegistry.deploy();
   await revocationRegistry.waitForDeployment();
   const revocationRegistryAddress = await revocationRegistry.getAddress();
-  console.log("RevocationRegistry deployed to:", revocationRegistryAddress);
 
   // Deploy VerificationLog
   const VerificationLog = await hre.ethers.getContractFactory("VerificationLog");
   const verificationLog = await VerificationLog.deploy();
   await verificationLog.waitForDeployment();
   const verificationLogAddress = await verificationLog.getAddress();
-  console.log("VerificationLog deployed to:", verificationLogAddress);
 
   // Deploy CredentialRegistry with constructor arguments
   const CredentialRegistry = await hre.ethers.getContractFactory("CredentialRegistry");
   const credentialRegistry = await CredentialRegistry.deploy();
   await credentialRegistry.waitForDeployment();
   const credentialRegistryAddress = await credentialRegistry.getAddress();
-  console.log("CredentialRegistry deployed to:", credentialRegistryAddress);
+  console.log("Contract Addresss:", credentialRegistryAddress);
 
   // Prepare the addresses object
   const deployedAddresses = {
